2f045a2d1439ce41e6ffbe74cb68964aef4327c1
Bozhin Karaivanov <bozhink@gmail.com>
2019-12-22 17:32:19 +0200

    Uppdate atlas-iot.go to calculate missing DP & HI

--- atlas-iot-go/src/atlas-iot.go
+++ atlas-iot-go/src/atlas-iot.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -16,8 +17,54 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// See https://en.wikipedia.org/wiki/Dew_point
+
 const port int = 60135
 const absoluteZeroC = -273.15
+
+// Dew point constants
+const a float64 = 6.1121 // mbar
+const b float64 = 18.678 // NA
+const c float64 = 257.14 // °C
+const d float64 = 234.5  // °C
+
+// Heat index constants
+const c1 float64 = -8.78469475556
+const c2 float64 = 1.61139411
+const c3 float64 = 2.33854883889
+const c4 float64 = -0.14611605
+const c5 float64 = -0.012308094
+const c6 float64 = -0.016424827778
+const c7 float64 = 0.002211732
+const c8 float64 = 0.00072546
+const c9 float64 = -0.000003582
+
+func gamma(t float64, h float64) float64 {
+	return math.Log(h/100.0) + (b*t)/(c+t)
+}
+
+// Get temperature of the dew point in °C
+func dp(t float64, h float64) float64 {
+	g := gamma(t, h)
+	return c * g / (b - g)
+}
+
+// Get the saturated water vapor pressure
+func ps(t float64, h float64) float64 {
+	return a * math.Exp((b*t)/(c+t))
+}
+
+// Get the actual vapor pressure
+func pa(t float64, h float64) float64 {
+	return (h / 100.0) * ps(t, h)
+}
+
+// Get the heat index
+func hi(t float64, h float64) float64 {
+	t2 := t * t
+	h2 := h * h
+	return c1 + c2*t + c3*h + c4*t*h + c5*t2 + c6*h2 + c7*t2*h + c8*t*h2 + c9*t2*h2
+}
 
 var client *mongo.Client
 
@@ -30,6 +77,10 @@ type Reading struct {
 	DewPoint    *float32 `json:"dewpoint,omitempty" bson:"dewpoint,omitempty"`
 	Pressure    *float32 `json:"pressure,omitempty" bson:"pressure,omitempty"`
 	Altitude    *float32 `json:"altitude,omitempty" bson:"altitude,omitempty"`
+	dp          *float32 `bson:"dp,omitempty"`
+	ps          *float32 `bson:"ps,omitempty"`
+	pa          *float32 `bson:"pa,omitempty"`
+	hi          *float32 `bson:"hi,omitempty"`
 }
 
 // Event data
@@ -77,6 +128,22 @@ func InsertRecordEndpoint(response http.ResponseWriter, request *http.Request) {
 				reading.Humidity = nil
 				reading.DewPoint = nil
 				reading.HeatIndex = nil
+			}
+
+			if reading.Humidity != nil && reading.Temperature != nil {
+				t := (float64)(*reading.Temperature)
+				h := (float64)(*reading.Humidity)
+				*reading.dp = (float32)((int)(dp(t, h)*100)) / 100.0
+				*reading.ps = (float32)((int)(ps(t, h)*100)) / 100.0
+				*reading.pa = (float32)((int)(pa(t, h)*100)) / 100.0
+				*reading.hi = (float32)((int)(hi(t, h)*100)) / 100.0
+				if reading.DewPoint == nil {
+					*reading.DewPoint = *reading.dp
+				}
+
+				if reading.HeatIndex == nil {
+					*reading.HeatIndex = *reading.hi
+				}
 			}
 		}
 
