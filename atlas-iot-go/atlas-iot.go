3b4531ee3cdd965c544e24389f7d5561a0c81dfe
Bozhin Karaivanov <bozhink@gmail.com>
2019-12-22 16:33:56 +0200

    Update Reading struct

--- atlas-iot-go/atlas-iot.go
+++ atlas-iot-go/atlas-iot.go
@@ -17,16 +17,19 @@ import (
 )
 
 const port int = 60135
+const absoluteZeroC = -273.15
 
 var client *mongo.Client
 
 // Reading data structure
 type Reading struct {
-	Sensor      string  `json:"sensor,omitempty" bson:"sensor,omitempty"`
-	Humidity    float32 `json:"humidity" bson:"humidity"`
-	Temperature float32 `json:"temperature" bson:"temperature"`
-	HeatIndex   float32 `json:"heatindex" bson:"heatindex"`
-	DewPoint    float32 `json:"dewpoint" bson:"dewpoint"`
+	Sensor      string   `json:"sensor,omitempty" bson:"sensor,omitempty"`
+	Humidity    *float32 `json:"humidity,omitempty" bson:"humidity,omitempty"`
+	Temperature *float32 `json:"temperature,omitempty" bson:"temperature,omitempty"`
+	HeatIndex   *float32 `json:"heatindex,omitempty" bson:"heatindex,omitempty"`
+	DewPoint    *float32 `json:"dewpoint,omitempty" bson:"dewpoint,omitempty"`
+	Pressure    *float32 `json:"pressure,omitempty" bson:"pressure,omitempty"`
+	Altitude    *float32 `json:"altitude,omitempty" bson:"altitude,omitempty"`
 }
 
 // Event data
@@ -59,11 +62,33 @@ func InsertRecordEndpoint(response http.ResponseWriter, request *http.Request) {
 	event.Date = time.Now()
 	log.Println(event)
 
-	collection := client.Database("iot").Collection("events")
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	result, _ := collection.InsertOne(ctx, event)
+	n := len(event.Readings)
+	if n > 0 {
 
-	json.NewEncoder(response).Encode(result)
+		for i := 0; i < n; i++ {
+			reading := event.Readings[i]
+			if *reading.Temperature < absoluteZeroC {
+				reading.Temperature = nil
+				reading.DewPoint = nil
+				reading.HeatIndex = nil
+			}
+
+			if *reading.Humidity < 0 || *reading.Humidity > 100 {
+				reading.Humidity = nil
+				reading.DewPoint = nil
+				reading.HeatIndex = nil
+			}
+		}
+
+		collection := client.Database("iot").Collection("events")
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		result, _ := collection.InsertOne(ctx, event)
+
+		json.NewEncoder(response).Encode(result)
+	} else {
+		log.Println("Invalid readings")
+		json.NewEncoder(response).Encode(nil)
+	}
 }
 
 func main() {
@@ -78,9 +103,10 @@ func main() {
 	uri := os.Args[1]
 	clientOptions := options.Client().ApplyURI(uri)
 
-	client, err = mongo.Connect(ctx, clientOptions)
-	if err != nil {
-		log.Fatal(err)
+	var e error
+	client, e = mongo.Connect(ctx, clientOptions)
+	if e != nil {
+		log.Fatal(e)
 	}
 
 	router := mux.NewRouter()
