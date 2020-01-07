eb4e506f495b52c74515528ecef8389c872dc045
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-01-07 15:30:03 +0200

    Update bmp388iot.ino

--- bmp388iot/bmp388iot.ino
+++ bmp388iot/bmp388iot.ino
@@ -16,11 +16,18 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+const String sensor = String(IOT_SENSOR);
+const String sender = String(IOT_USER_AGENT);
+const String event = String(IOT_EVENT);
+const String version = String(IOT_VERSION);
+
 ESP8266WiFiMulti WiFiMulti;
 
 Adafruit_BMP3XX bmp; // I2C
 
 bool configureSensor();
+String getReading();
+String getPayload(String reading);
 
 void setup()
 {
@@ -50,60 +57,47 @@ void loop()
     // wait for WiFi connection
     if ((WiFiMulti.run() == WL_CONNECTED))
     {
-        if (!bmp.performReading())
+        String reading = getReading();
+
+        if (reading.length() > 2) // The reading must be valid JSON object.
         {
-            USE_SERIAL.println("Failed to perform reading :(");
-            return;
-        }
-        USE_SERIAL.print("Temperature = ");
-        USE_SERIAL.print(bmp.temperature);
-        USE_SERIAL.println(" *C");
+            String payload = getPayload(reading);
+            USE_SERIAL.println(payload);
 
-        USE_SERIAL.print("Pressure = ");
-        USE_SERIAL.print(bmp.pressure / 100.0);
-        USE_SERIAL.println(" hPa");
+            HTTPClient http;
 
-        USE_SERIAL.print("Approx. Altitude = ");
-        USE_SERIAL.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-        USE_SERIAL.println(" m");
+            USE_SERIAL.print("[HTTP] begin...\n");
+            // configure target server and url
+            //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+            http.begin(IOT_API_URI); //HTTP
+            http.addHeader(CONTENT_TYPE_HTTP_HEADER_NAME, CONTENT_TYPE_HTTP_HEADER_VALUE);
+            http.addHeader(USER_AGENT_HTTP_HEADER_NAME, IOT_USER_AGENT);
+            http.addHeader(IOT_EVENT_HTTP_HEADER_NAME, IOT_EVENT);
+            http.addHeader(IOT_VERSION_HTTP_HEADER_NAME, IOT_VERSION);
 
-        USE_SERIAL.println();
-        delay(2000);
+            USE_SERIAL.print("[HTTP] POST...\n");
+            // start connection and send HTTP header
+            int httpCode = http.POST(payload);
 
-        HTTPClient http;
-
-        USE_SERIAL.print("[HTTP] begin...\n");
-        // configure target server and url
-        //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
-        http.begin(IOT_API_URI); //HTTP
-        http.addHeader(CONTENT_TYPE_HTTP_HEADER_NAME, CONTENT_TYPE_HTTP_HEADER_VALUE);
-        http.addHeader(USER_AGENT_HTTP_HEADER_NAME, IOT_USER_AGENT);
-        http.addHeader(IOT_EVENT_HTTP_HEADER_NAME, IOT_EVENT);
-        http.addHeader(IOT_VERSION_HTTP_HEADER_NAME, IOT_VERSION);
-
-        USE_SERIAL.print("[HTTP] POST...\n");
-        // start connection and send HTTP header
-        int httpCode = http.POST("{}");
-
-        // httpCode will be negative on error
-        if (httpCode > 0)
-        {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
-
-            // file found at server
-            if (httpCode == HTTP_CODE_OK)
+            // httpCode will be negative on error
+            if (httpCode > 0)
             {
-                String payload = http.getString();
-                USE_SERIAL.println(payload);
-            }
-        }
-        else
-        {
-            USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
+                // HTTP header has been send and Server response header has been handled
+                USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
 
-        http.end();
+                // file found at server
+                if (httpCode == HTTP_CODE_OK)
+                {
+                    USE_SERIAL.println(http.getString());
+                }
+            }
+            else
+            {
+                USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            }
+
+            http.end();
+        }
     }
 
     delay(10000);
@@ -122,4 +116,23 @@ bool configureSensor()
     }
 
     return false;
+}
+
+String getReading()
+{
+    if (bmp.performReading())
+    {
+        const String temperature = String(bmp.temperature, 2);                     /* [*C] */
+        const String pressure = String(bmp.pressure / 100.0, 2);                   /* [hPa] */
+        const String altitude = String(bmp.readAltitude(SEALEVELPRESSURE_HPA), 2); /* [m] */
+
+        return "{\"sensor\":\"" + sensor + "\",\"temperature\":" + temperature + ",\"pressure\":" + pressure + ",\"altitude\":" + altitude + "}";
+    }
+
+    return "";
+}
+
+String getPayload(String reading)
+{
+    return "{\"sender\":\"" + sender + "\",\"event\":\"" + event + "\",\"readings\":[" + reading + "],\"version\":\"" + version + "\"}";
 }
