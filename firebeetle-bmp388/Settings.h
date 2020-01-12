29ba7df2ec9d776e8372a99020a10d2c58b25d1c
Bozhin Karaivanov <bozhin.karaivanov@gmail.com>
2020-01-12 14:25:07 +0200

    Update firebeetle-bmp388

--- firebeetle-bmp388/Settings.h
+++ firebeetle-bmp388/Settings.h
@@ -1,5 +1,6 @@
 #define IOT_API_URI "http://192.168.5.87:60135/api/v1/events"
 #define IOT_USER_AGENT "FireBeetle"
 #define IOT_EVENT "readings"
-#define IOT_VERSION "0.0.2"
+#define IOT_VERSION "0.1.0"
 #define IOT_SENSOR "BMP388"
+#define NUMBER_OF_READINGS 1
--- firebeetle-bmp388/firebeetle-bmp388.ino
+++ firebeetle-bmp388/firebeetle-bmp388.ino
@@ -31,108 +31,120 @@ String getPayload(String reading);
 
 void setup()
 {
-    // USE_SERIAL.begin(115200);
-    // while (!USE_SERIAL)
-    // {
-    // }
-    // USE_SERIAL.setDebugOutput(true);
+  // USE_SERIAL.begin(115200);
+  // while (!USE_SERIAL)
+  // {
+  // }
+  // USE_SERIAL.setDebugOutput(true);
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
-    if (!configureSensor())
+  if (!configureSensor())
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    while (1)
     {
-        digitalWrite(LED_BUILTIN, HIGH);
-        while (1)
-        {
-            // Loop forever
-        }
+      // Loop forever
     }
+  }
 
-    WiFiMulti.addAP(STASSID, STAPASS);
+  WiFiMulti.addAP(STASSID, STAPASS);
+  delay(10000);
 }
 
 void loop()
 {
-    // wait for WiFi connection
-    if ((WiFiMulti.run() == WL_CONNECTED))
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED))
+  {
+    for (int nr = 0; nr < NUMBER_OF_READINGS; nr++)
     {
-        String reading = getReading();
+      String reading = getReading();
 
-        if (reading.length() > 2) // The reading must be valid JSON object.
-        {
-            String payload = getPayload(reading);
-            // USE_SERIAL.println(payload);
+      // Skip the first warming reading.
+      delay(3000);
+      reading = getReading();
 
-            HTTPClient http;
+      if (reading.length() > 2) // The reading must be valid JSON object.
+      {
+        String payload = getPayload(reading);
+        // USE_SERIAL.println(payload);
 
-            // USE_SERIAL.print("[HTTP] begin...\n");
-            // configure target server and url
-            //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
-            http.begin(IOT_API_URI); //HTTP
-            http.addHeader(CONTENT_TYPE_HTTP_HEADER_NAME, CONTENT_TYPE_HTTP_HEADER_VALUE);
-            http.addHeader(USER_AGENT_HTTP_HEADER_NAME, IOT_USER_AGENT);
-            http.addHeader(IOT_EVENT_HTTP_HEADER_NAME, IOT_EVENT);
-            http.addHeader(IOT_VERSION_HTTP_HEADER_NAME, IOT_VERSION);
+        HTTPClient http;
 
-            // USE_SERIAL.print("[HTTP] POST...\n");
-            // start connection and send HTTP header
-            int httpCode = http.POST(payload);
+        // USE_SERIAL.print("[HTTP] begin...\n");
+        // configure target server and url
+        //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+        http.begin(IOT_API_URI); //HTTP
+        http.addHeader(CONTENT_TYPE_HTTP_HEADER_NAME, CONTENT_TYPE_HTTP_HEADER_VALUE);
+        http.addHeader(USER_AGENT_HTTP_HEADER_NAME, IOT_USER_AGENT);
+        http.addHeader(IOT_EVENT_HTTP_HEADER_NAME, IOT_EVENT);
+        http.addHeader(IOT_VERSION_HTTP_HEADER_NAME, IOT_VERSION);
 
-            // // httpCode will be negative on error
-            // if (httpCode > 0)
-            // {
-            //     // HTTP header has been send and Server response header has been handled
-            //     // USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
+        // USE_SERIAL.print("[HTTP] POST...\n");
+        // start connection and send HTTP header
+        int httpCode = http.POST(payload);
 
-            //     // file found at server
-            //     if (httpCode == HTTP_CODE_OK)
-            //     {
-            //         // USE_SERIAL.println(http.getString());
-            //     }
-            // }
-            // else
-            // {
-            //     // USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-            // }
+        // // httpCode will be negative on error
+        // if (httpCode > 0)
+        // {
+        //     // HTTP header has been send and Server response header has been handled
+        //     // USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
 
-            http.end();
-        }
+        //     // file found at server
+        //     if (httpCode == HTTP_CODE_OK)
+        //     {
+        //         // USE_SERIAL.println(http.getString());
+        //     }
+        // }
+        // else
+        // {
+        //     // USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        // }
+
+        http.end();
+      }
+
+      ESP.deepSleep(5 * 60e6);
     }
-
-    delay(30000);
+  }
+  else
+  {
+    delay(10000);
+  }
 }
 
 bool configureSensor()
 {
-    if (bmp.begin())
-    {
-        // Set up oversampling and filter initialization
-        bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-        bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-        bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-        //bmp.setOutputDataRate(BMP3_ODR_50_HZ);
-        return true;
-    }
+  if (bmp.begin())
+  {
+    // Set up oversampling and filter initialization
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    //bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 String getReading()
 {
-    if (bmp.performReading())
-    {
-        const String temperature = String(bmp.temperature, 2);                     /* [*C] */
-        const String pressure = String(bmp.pressure / 100.0, 2);                   /* [hPa] */
-        const String altitude = String(bmp.readAltitude(SEALEVELPRESSURE_HPA), 2); /* [m] */
+  if (bmp.performReading())
+  {
+    const String temperature = String(bmp.temperature, 2);                     /* [*C] */
+    const String pressure = String(bmp.pressure / 100.0, 2);                   /* [hPa] */
+    const String altitude = String(bmp.readAltitude(SEALEVELPRESSURE_HPA), 2); /* [m] */
 
-        return "{\"sensor\":\"" + sensor + "\",\"temperature\":" + temperature + ",\"pressure\":" + pressure + ",\"altitude\":" + altitude + "}";
-    }
+    return "{\"sensor\":\"" + sensor + "\",\"temperature\":" + temperature + ",\"pressure\":" + pressure + ",\"altitude\":" + altitude + "}";
+  }
 
-    return "";
+  return "";
 }
 
 String getPayload(String reading)
 {
-    return "{\"sender\":\"" + sender + "\",\"event\":\"" + event + "\",\"readings\":[" + reading + "],\"version\":\"" + version + "\"}";
+  return "{\"sender\":\"" + sender + "\",\"event\":\"" + event + "\",\"readings\":[" + reading + "],\"version\":\"" + version + "\"}";
 }
